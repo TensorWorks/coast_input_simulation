75831bf34c38ed483f1dd0ef9abcaae0ba93451a
Nicholas Pace <nicholas.pace@live.com>
2022-03-29 13:39:24 +1100

    Add some comments to test to describe timing of tests

--- input_sim_test.cc
+++ input_sim_test.cc
@@ -9,6 +9,7 @@ void keyboard_test()
 {
     fprintf(stdout, "Starting Keyboard Test\n");
 
+    // 0 ms
     Coast::SendInput(Coast::KeyEvent{true, Coast::KeyCodes::COAST_LeftMeta});
     std::this_thread::sleep_for(std::chrono::milliseconds(25));
     Coast::SendInput(Coast::KeyEvent{false, Coast::KeyCodes::COAST_LeftMeta});
@@ -70,19 +71,22 @@ void keyboard_test()
     Coast::SendInput(Coast::KeyEvent{false, Coast::KeyCodes::COAST_3});
     std::this_thread::sleep_for(std::chrono::milliseconds(25));
     Coast::SendInput(Coast::KeyEvent{true, Coast::KeyCodes::COAST_2});
-    std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    std::this_thread::sleep_for(std::chrono::milliseconds(25)); 
     Coast::SendInput(Coast::KeyEvent{false, Coast::KeyCodes::COAST_2});
+    // 1250 ms
 
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     Coast::SendInput(Coast::KeyEvent{true, Coast::KeyCodes::COAST_Escape});
     std::this_thread::sleep_for(std::chrono::milliseconds(25));
     Coast::SendInput(Coast::KeyEvent{false, Coast::KeyCodes::COAST_Escape});
+    // 4275 ms
 }
 
 void mouse_test()
 {
     fprintf(stdout, "Starting Mouse Absolute Position Test\n");
 
+    // 0 ms
     Coast::SendInput(Coast::MousePositionAbsoluteEvent{50, 50});
     std::this_thread::sleep_for(std::chrono::milliseconds(5));
 
@@ -92,6 +96,7 @@ void mouse_test()
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     Coast::SendInput(Coast::MouseButtonEvent{true, Coast::MouseButtons::LEFT});
     std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    // 1055 ms
 
     for(int i = 0; i < 360; i++) {
         const int offset = 300;
@@ -106,6 +111,7 @@ void mouse_test()
 
     Coast::SendInput(Coast::MouseButtonEvent{false, Coast::MouseButtons::LEFT});
     std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    // 6480 ms
 
     fprintf(stdout, "Starting Mouse Relative Position Test\n");
 
@@ -123,6 +129,7 @@ void mouse_test()
     
     Coast::SendInput(Coast::MouseButtonEvent{false, Coast::MouseButtons::LEFT});
     std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    // 10530 ms
 }
 
 int main(int argc, char* argv[]) {
